e490aaccd3360e616291fc52dbcbd574891930fe
brunakrodrigues <brunakjrodrigues@gmail.com>
2023-12-30 03:13:30 -0300

    update start-free css

--- src/components/StartFree/index.tsx
+++ src/components/StartFree/index.tsx
@@ -5,19 +5,30 @@ import { FaArrowRight } from "react-icons/fa6";
 
 export default function StartFree() {
   return (
-    <Box bg="primary" py={8} bgImage="url('/bg-start.png')" // substitua com o caminho para a sua imagem
-    bgPos="center" // centraliza a imagem de fundo
-    bgRepeat="no-repeat" // impede a repetição da imagem
-    bgSize="cover" >
+    <Box
+      bg="primary"
+      py={8}
+      bgImage="url('/bg-start.png')"
+      bgPos="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
       <Box textAlign="center" fontWeight={500} py="4rem">
         <Heading color="white" fontSize="5xl">Para começar, é grátis. </Heading>
         <Text color="gray.100" fontWeight={400} letterSpacing={1}> Inscreva-se e dê o primeiro passo para o seu crescimento online.</Text>
       </Box>
       <Flex justifyContent="center" alignItems="center" h="100%">
-        <Button mx="2" bg="primary" color="white" variant="outline">
+        <Button mx="2" bg="primary" color="white" variant="outline" _hover={{bg: "#1961a4"}}>
           Ver planos
         </Button>
-        <Button mx="2" bg="black" color="white" variant="solid" rightIcon={<FaArrowRight />}>
+        <Button
+          mx="2"
+          bg="black"
+          color="white"
+          variant="solid"
+          rightIcon={<FaArrowRight />}
+          _hover={{bg: "gray.500"}}
+        >
           Comece agora
         </Button>
       </Flex>
